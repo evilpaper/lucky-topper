3476be32b82e02595f6df062a1baaa780f55d773
Pelle Lundgren <pelle.lundgren@bubbi.ai>
2023-07-14 16:54:35 +0200

    Init git, add first commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// Line above specifies that the file must be executed by npx and ts-node
+
+const currentDateAndTime = new Date().toISOString();
+
+console.log(currentDateAndTime);
